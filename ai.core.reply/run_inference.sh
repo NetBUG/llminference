1c1d61becbfb85f33290ee233311bb69b9520af6
NetBUG <netbug@bugbook3.local>
2025-02-20 17:04:07 +0100

    Skeleton for Docker

---
+++ ai.core.reply/run_inference.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ai.core.reply
+
+docker build . -t llm_inferrer
+
+# Determine if CUDA devices is available
+if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
+    echo "CUDA_VISIBLE_DEVICES is not set. Running on CPU."
+    docker run -p 8000:80 --name ai_core llm_inferrer:latest
+else
+    echo "CUDA_VISIBLE_DEVICES is set. Running on GPU."
+    docker run --gpus 1 --ipc host -p 8000:80 --name ai_core llm_inferrer:latest
+fi
