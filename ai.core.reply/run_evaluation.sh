058b8488957a837760c11ba758d5f1c73acb8c5b
NetBUG <netbug@bugbook3.local>
2025-02-24 00:41:33 +0100

    TASK-5 Reuse container for tests

--- ai.core.reply/run_evaluation.sh
+++ ai.core.reply/run_evaluation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ai.core.reply
-
 docker build . -t llm_inferrer
 
-docker run --ipc host --gpus 0 -e LOGURU_LEVEL=INFO llm_inferrer python3 evaluation/evaluate.py -m trl
-docker run --ipc host --gpus 0 -e LOGURU_LEVEL=INFO llm_inferrer python3 evaluation/evaluate.py -m vllm
+docker rm -f ai_core_eval
+docker run --ipc host --gpus 0 -e LOGURU_LEVEL=INFO --name ai_core_eval llm_inferrer python3 evaluation/evaluate.py -m trl
+docker commit ai_core_eval ai_core_eval_1
+docker run --ipc host --gpus 0 -e LOGURU_LEVEL=INFO --name ai_core_eval_1 ai_core_eval_1 python3 evaluation/evaluate.py -m vllm
